6180ec6ce414d50e4090f18b6cb4f0093cdeaa32
yehezkielgunawan <yehezkiel.gunawan@student.umn.ac.id>
2022-06-08 20:14:09 +0700

    chore(package.json): :arrow_up: update deps

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,10 @@
 import "@/styles/globals.css";
 import { ThemeProvider } from "next-themes";
-import type { AppProps } from "next/app";
+
+
+
+import type {AppProps} from "next/app";
+
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
